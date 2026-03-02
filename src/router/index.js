9faf5dd0221bb2454ca863764c6db375a35488e1
Lucas <lucascsvet@gmail.com>
2026-03-02 09:31:59 -0300

    US04 - Página de login público

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/login', name: 'login', component: () => import('../views/LoginView.vue'), meta: { guest: true } },
+  { path: '/acesso/:id', name: 'public-login', component: () => import('../views/PublicLoginView.vue'), props: true },
   { path: '/registro', name: 'register', component: () => import('../views/RegisterView.vue'), meta: { guest: true } },
   { path: '/', name: 'dashboard', component: () => import('../views/DashboardView.vue'), meta: { requiresAuth: true } },
   { path: '/:pathMatch(.*)*', redirect: '/' },
